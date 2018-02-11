4865bda5ce531862f09f508d6781aa8fad6dbad4
peter epsteen <peterepsteen@Jennifers-iMac.home>
2018-02-11 12:15:37 -0800

    Mobile and some links

---
+++ js/site.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggleNav').addEventListener('click', () => {
+    document.querySelector('nav > ul').classList.toggle('open');
+    document.querySelector('#nav-icon4').classList.toggle('open');
+});
